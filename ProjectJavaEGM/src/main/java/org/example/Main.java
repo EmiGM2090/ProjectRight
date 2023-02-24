52643b0f6f3c378901e6480b3ef8e07825571a04
EmiGM2090 <godinezemiliano2015@gmail.com>
2023-02-24 10:40:26 -0600

    Practica 1

--- ProjectJavaEGM/src/main/java/org/example/Main.java
+++ ProjectJavaEGM/src/main/java/org/example/Main.java
@@ -35,7 +35,7 @@ public class Main {
 
     //Desarrollar un programa que le pida al usuario su nombre, su edad, su peso y su altura y que calcule el imc y al final muestren una ficha de todos sus datos
 
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
 
         String nombre;
         int edad;
@@ -63,6 +63,69 @@ public class Main {
         System.out.println("Edad: " + edad);
         System.out.println("Peso: " + peso);
         System.out.println("Altura: " + altura);
-        System.out.println("IMC: " + imc);
+        System.out.println("IMC: " + imc);*/
+
+        //Desarrollar un programa que le pida al usuario su nombre, su apellido, su domicilio, cuanto cotizo en 2022, y cuanto impuesto va a pagar y el total aparte.
+        //Declarar constante de ISR = 12% de impuestos
+
+        /*Scanner scanner = new Scanner(System.in);
+
+        String nombre;
+        String apellido;
+        String domicilio;
+        double cotizo;
+        double impuestos;
+        final double isr = 0.12;
+
+        System.out.println("Inserte su nombre");
+        nombre = scanner.nextLine();
+
+        System.out.println("Inserte sus apellidos");
+        apellido = scanner.nextLine();
+
+        System.out.println("Inserte su domicilio");
+        domicilio = scanner.nextLine();
+
+        System.out.println("Inserte cuanto cotizo en el 2022");
+        cotizo = scanner.nextDouble();
+
+        impuestos = cotizo * isr;
+
+        System.out.println("Su impuesto es: $" + impuestos);*/
+
+
+        //Paso 1 Pedir al usuario su nombre, apellido, domicilio, y el precio de su carro
+        //paso 2 Calcular el impuesto de tenencia (1.3% del precio del carro)
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nombre;
+        String apellido;
+        String domicilio;
+        double carro;
+        double impuesto;
+        final double tenencia = 0.013;
+
+        System.out.println("Inserte su nombre");
+        nombre = scanner.nextLine();
+
+        System.out.println("Inserte apellido");
+        apellido = scanner.nextLine();
+
+        System.out.println("Inserte domicilio");
+        domicilio = scanner.nextLine();
+
+        System.out.println("inserte precio de su carro");
+        carro = scanner.nextDouble();
+
+        impuesto = carro * tenencia;
+
+        System.out.println("Su nombre es: " + nombre);
+        System.out.println("Sus apellidos son: " + apellido);
+        System.out.println("Su domicilio es: " + domicilio);
+        System.out.println("El precio del carro es: $" + carro);
+        System.out.println("El impuesto de tenencia es: $" + impuesto);
+
+        //Tema:
     }
 }
