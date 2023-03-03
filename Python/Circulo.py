5baf904d7fbb30cdeccff42274af5e88e12b4636
EmiGM2090 <godinezemiliano2015@gmail.com>
2023-03-03 11:13:53 -0600

    Ultima actualizacion el viernes 03/03/23

---
+++ Python/Circulo.py
@@ -0,0 +1,9 @@
+r = input ("Inserte el radio del circulo ")
+
+r = int (r)
+
+p = 3.1416
+
+area = p * (r * r)
+
+print ("El area del circulo es: ", area)
---
+++ Python/Triangulo.py
@@ -0,0 +1,12 @@
+
+b = input ("Inserte la medida de la base del triangulo ")
+
+b = int (b)
+
+a = input ("Inserte la medida de la altura del triangulo ")
+
+a = int (a)
+
+area = (b * a) / 2
+
+print ("EL area del triangulo es: ", area)
